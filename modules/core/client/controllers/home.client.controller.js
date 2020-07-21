b6e7c3e730d2379f6246c5190f7613e190c22a95
Joe Chen <ychen259@ufl.edu>
2020-07-21 01:26:18 -0700

    fix the header

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -3,41 +3,4 @@
     function ($scope, $location, $stateParams, $state ){
     var vm = this;
 
-
-      /* Map properties */
-    $scope.map = {
-      center: {
-        latitude: 34.151570,
-        longitude: -118.005770
-      }, 
-      zoom: 14
-    }
-
-
-
-
-    $scope.options = {
-        scrollwheel: false
-    };
-
-    $scope.onClick = function(marker, eventName, model) {
-        model.show = !model.show;
-    };
-
-    var listing = {
-    	        id: 123,
-                latitude: 34.151570,
-                longitude: -118.005770,
-                name: "Aztec Hotel", 
-                code: 91016, 
-                address: "311 W Foothill blvd, Monrovia, CA, 91016",
-                show: false 
-    }
-
-    var temp = [];
-    temp.push(listing);
-    $scope.markers = temp;
-
-
-
 }]);
