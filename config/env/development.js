855bf14af850308be476299e3034c28f9e935b58
Joe Chen <ychen259@ufl.edu>
2020-07-31 21:32:46 -0700

    setting env for heroku

--- config/env/development.js
+++ config/env/development.js
@@ -60,12 +60,24 @@ module.exports = {
     sandbox: true
   },
   mailer: {
+    /*
     from: process.env.MAILER_FROM || 'MAILER_FROM',
     options: {
       service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
       auth: {
         user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
         pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
+      }
+    }*/
+    from: process.env.MAILER_FROM || 'MAILER_FROM' || 'aztechotelwebsite@gmail.com',
+    to: "yuzhuochen12@gmail.com",
+    options: {
+      service: 'Gmail',
+      secure: false,
+      port:465,
+      auth: {
+        user: 'aztechotelwebsite@gmail.com',
+        pass: 'aztechotel'
       }
     }
   },
